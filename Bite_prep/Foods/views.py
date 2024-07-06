b376f7a4627106e3a8bbab1a90ebc61a8753e30e
Adaskox <adam.martka@gmail.com>
2024-07-06 23:47:39 +0200

    [ADD] added basic views

--- Bite_prep/Foods/views.py
+++ Bite_prep/Foods/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+
+from .models import FoodCategory, Food, Nutrient, FoodNutrient
 
 
 def index(request):
-    return HttpResponse('Homepage')
+    foods = Food.name
+    template = loader.get_template('foods/index.html')
+    context = {
+        'foods': foods,
+    }
+    return HttpResponse(template.render(context, request))
