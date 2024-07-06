13e2b5241d1413299e80341b77c9564c8027986d
Adaskox <adam.martka@gmail.com>
2024-07-06 14:09:34 +0200

    [FIX] change app name to more appropriate one

--- Bite_prep/Bite_prep/settings.py
+++ Bite_prep/Bite_prep/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'backend.apps.BackendConfig',
+    'Foods.apps.FoodsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
