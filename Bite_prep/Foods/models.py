e128981852742728c0110d01f3ae8bb81679a1da
Adaskox <adam.martka@gmail.com>
2024-07-07 14:58:45 +0200

    [ADD][FIX] added views with logic for listing all foods, fixed models a bit

--- Bite_prep/Foods/models.py
+++ Bite_prep/Foods/models.py
@@ -13,6 +13,7 @@ class Food(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(FoodCategory, on_delete=models.SET_NULL, null=True, related_name='foods')
     description = models.TextField(blank=True, null=True)
+    calories_100g = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
@@ -29,10 +30,11 @@ class Nutrient(models.Model):
 class FoodNutrient(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='food_nutrients')
     nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
-    amount = models.FloatField()
+    amount_100g = models.FloatField(default=0)
 
     class Meta:
         unique_together = ('food', 'nutrient')
 
     def __str__(self):
-        return f"{self.amount} {self.nutrient.unit} of {self.nutrient.name} in {self.food.name}"
+        return f"{self.amount_100g} {self.nutrient.unit} of {self.nutrient.name} in 100g of {self.food.name}"
+    
--- Bite_prep/Foods/views.py
+++ Bite_prep/Foods/views.py
@@ -4,8 +4,18 @@ from .models import FoodCategory, Food, Nutrient, FoodNutrient
 
 
 def index(request):
-    foods_list = Food.objects.all()
-    context = {
+    portion_size = request.GET.get('portion_size,', 100)
+    portion_size = float(portion_size)
+
+    foods_list = Food.objects.all().prefetch_related('food_nutrients', 'category')
+
+    for food in foods_list:
+        food.calories = food.calories_100g * portion_size / 100
+        for food_nutrient in food.food_nutrients.all():
+            food_nutrient.amount = food_nutrient.amount_100g * portion_size / 100
+
+    context= {
         'foods_list': foods_list,
+        'portion_size': portion_size,
     }
     return render(request, 'Foods/index.html', context)
